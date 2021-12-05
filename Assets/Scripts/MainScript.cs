2e5d301654508868fa7d90e4902710c1717db356
ibrahwem <wattad111@gmail.com>
2021-12-05 15:22:03 +0200

    fix car through bugs and adding new character

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -9,4 +9,8 @@ public class MainScript : MonoBehaviour
     public void StartButton(){
         SceneManager.LoadScene("Game_Field");
     }
+    public void ExitButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
 }
