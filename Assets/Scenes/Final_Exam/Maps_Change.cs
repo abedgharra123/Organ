b85711eeed199ae35759b1f7b0db303ccf8e5f10
ibrahwem <wattad111@gmail.com>
2022-03-24 16:18:52 +0200

    Fixing the final exam break_heart and start particles bug

--- Assets/Scenes/Final_Exam/Maps_Change.cs
+++ Assets/Scenes/Final_Exam/Maps_Change.cs
@@ -29,34 +29,27 @@ public class Maps_Change : MonoBehaviour
     {
         if (col.collider.tag == "Kol_Transform")
         {
-            History_Map.SetActive(true);
-            transform.position = new Vector3(3, 10.92f, 52.75f);
-            Torat_Kol_Map.SetActive(false);
+            Invoke("Kol_Transform", 1);
         }
         if (col.collider.tag == "History_Transform")
         {
-            Math_Map.SetActive(true);
-            transform.position = new Vector3(3, 21, 102.32f);
-            History_Map.SetActive(false);
+            Invoke("History_Transform", 1);
         }
         if (col.collider.tag == "Math_Transform")
         {
-            Physics_Map.SetActive(true);
-            transform.position = new Vector3(3, 30.947f, 152.468f);
-            Math_Map.SetActive(false);
+            Invoke("Math_Transform", 1);
 
         }
         if (col.collider.tag == "Correct_Answer")
         {
             Star.SetActive(true);
-            final_kol_score++;
-            score_txt.text = "" + final_kol_score;
+            Invoke("Correct_Answer", 1);
+
         }
         if (col.collider.tag == "Wrong_Answer")
         {
             Brocken_Heart.SetActive(true);
-
-
+            Invoke("Wrong_Answer", 1.5f);
         }
 
 
@@ -65,7 +58,35 @@ public class Maps_Change : MonoBehaviour
 
     }
 
+    public void Kol_Transform()
+    {
+        History_Map.SetActive(true);
+        transform.position = new Vector3(3, 10.92f, 52.75f);
+        Torat_Kol_Map.SetActive(false);
+    }
+    public void History_Transform()
+    {
+            Math_Map.SetActive(true);
+            transform.position = new Vector3(3, 21, 102.32f);
+            History_Map.SetActive(false);
+        
+    }
+    public void Math_Transform()
+    {
+        Physics_Map.SetActive(true);
+        transform.position = new Vector3(3, 30.947f, 152.468f);
+        Math_Map.SetActive(false);
+    }
 
-
+    public void Correct_Answer()
+    {
+        Star.SetActive(false);
+        final_kol_score++;
+        score_txt.text = "" + final_kol_score;
+    }
+    public void Wrong_Answer()
+    {
+            Brocken_Heart.SetActive(false);
+    }
 
 }
