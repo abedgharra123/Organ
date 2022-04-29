0b6c1f41920516f383bece68c88000afc64230c2
abedgharra123 <abed.gharra9.9@hotmail.com>
2022-04-29 21:36:11 +0300

    Cleaned music

--- Assets/Scripts/States/AllData.cs
+++ Assets/Scripts/States/AllData.cs
@@ -15,8 +15,14 @@ public class AllData : MonoBehaviour
     [SerializeField] List<State> MathStates;
     [SerializeField] GameObject Boy;
     [SerializeField] GameObject Girl;
+    [SerializeField] UnityEngine.Video.VideoPlayer Clip;
     private int index;
     // Start is called before the first frame update
+
+    enum StatesType{
+        BrainStates,
+        History,
+    }
     void Start()
     {
         //Title.text = states[0].GetTitle();
@@ -44,6 +50,7 @@ public class AllData : MonoBehaviour
         image.sprite = states[index].GetImage();
     }
     public void ExitScenes(){
+        Clip.Play();
         StatesUI.SetActive(false);
         PlayerField.SetActive(true);
     }
@@ -55,27 +62,36 @@ public class AllData : MonoBehaviour
             if (Physics.Raycast(raycast, out raycastHit)){
                 Debug.Log("Something Hit");
                 if (raycastHit.collider.CompareTag("BrainSound")){
-                    Debug.Log("BrainSound Hit");
-                    PlayerField.SetActive(false);
-                    states = BrainStates;
-                    Title.text = states[0].GetTitle();
-                    image.sprite = states[0].GetImage();
-                    index = 0;
-                    StatesUI.SetActive(true);
-
+                    OnCharacterClick(StatesType.BrainStates);
                 }
                 if (raycastHit.collider.CompareTag("History")){
-                    Debug.Log("History Hit");
-                    PlayerField.SetActive(false);
-                    states = HistoryStates;
-                    Title.text = states[0].GetTitle();
-                    image.sprite = states[0].GetImage();
-                    index = 0;
-                    StatesUI.SetActive(true);
+                    OnCharacterClick(StatesType.History);
                 }
             }
         }
     }
+
+    private void OnCharacterClick(StatesType type){
+        AudioManager.instance.Play("Click");
+        switch(type){
+            case StatesType.BrainStates:
+            states = BrainStates;
+            Debug.Log("Brain charachter");
+            break;
+            case StatesType.History:
+            Debug.Log("History charachter");
+            states = HistoryStates;
+            break;
+        }
+        PlayerField.SetActive(false);
+        Title.text = states[0].GetTitle();
+        image.sprite = states[0].GetImage();
+        index = 0;
+        StatesUI.SetActive(true);
+        Clip.Pause();
+    }
+
+
 
 
 }
